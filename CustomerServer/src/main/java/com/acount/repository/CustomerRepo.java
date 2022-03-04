e09f4ec97d3a47f17257d9b82b8a1b32c28a306d
Vibhu Raj <vibhuraj23@gmail.com>
2022-03-04 20:54:27 +0530

    Second

--- CustomerServer/src/main/java/com/acount/repository/CustomerRepo.java
+++ CustomerServer/src/main/java/com/acount/repository/CustomerRepo.java
@@ -1,9 +1,11 @@
 package com.acount.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.acount.entities.Customer;
 
+@Repository
 public interface CustomerRepo extends MongoRepository<Customer, Integer> {
 
 }
